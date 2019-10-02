86f96eb879c9bb5d406b866a0eaddc31b01987ba
Keisuke-Takagi <k19990410@gmail.com>
2019-10-02 17:30:24 +0900

    新規登録時のバリデーションとパスワードの置換

--- insert.php
+++ insert.php
@@ -17,10 +17,16 @@
   <?php
     $count = 0;
     $dbh = new PDO("mysql:host=127.0.0.1; dbname=record_book; charset=utf8", 'root', 'password');
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    echo $email;
-    echo $password;
+    if (!$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      echo '入力された値が不正です。';
+      return false;
+    }
+    if (preg_match('/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{5,20}+\z/i', $_POST['password'])) {
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    } else {
+      echo 'パスワードは半角英数字をそれぞれ1文字以上含んだ5文字以上で設定してください。';
+      return false;
+    }
     if(isset($email)){
       if(isset($password)){
         $db_emails = "SELECT email FROM users";
@@ -34,7 +40,6 @@
         if($count == 0){$sql = "INSERT INTO users (
           email, password) VALUES (:email,:password
         )";
-
         $stmt = $dbh->prepare($sql);
         $params = array(':email' => $email, ':password' => $password);
         $stmt->execute($params);
