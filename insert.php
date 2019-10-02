f2f2552a1c80b522753853b4288f4a3520f54139
Keisuke-Takagi <k19990410@gmail.com>
2019-10-02 15:04:51 +0900

    登録完了処理(条件分岐なし)

---
+++ insert.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
+  <link rel="stylesheet" type="text/css" href="./style.css"/>
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <!-- bootstrapの導入 -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <title>ユーザー追加ページ</title>
+</head>
+<body>
+  <?php
+    $dbh = new PDO("mysql:host=127.0.0.1; dbname=record_book; charset=utf8", 'root', 'password');
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    echo $email;
+    echo $password;
+    $sql = "INSERT INTO users (
+      email, password) VALUES (:email,:password
+    )";
+    $stmt = $dbh->prepare($sql);
+    $params = array(':email' => $email, ':password' => $password);
+    $stmt->execute($params);
+  ?>
+  <a href="registration.php">もどる</a>
+</body>
+</html>
